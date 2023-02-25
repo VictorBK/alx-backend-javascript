6ab21c4687787b5aee9ba2d25786cc3d08008eb8
VictorBK <victorbrian4@gmail.com>
2023-02-25 03:00:50 +0300

    Basic Integration testing

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,77 +1,19 @@
-'use strict';
 const request = require('request');
-const chai = require('chai');
+const { expect } = require('chai');
 
-describe('GET /', () => {
-  it('endpoint: GET /', (done) => {
-    const call = {
-      url: 'http://localhost:7865',
-      method: 'GET',
-    };
-    request(call, (error, response, body) => {
-      chai.expect(response.statusCode).to.equal(200);
-      chai.expect(body).to.equal('Welcome to the payment system');
-      done();
-    });
-  });
-});
+describe('Integration Testing', () => {
+  describe('GET /', () => {
+    it('Code: 200 | Body: Welcome to the payment system', (done) => {
+      const options = {
+        url: 'http://localhost:7865',
+        method: 'GET',
+      };
 
-describe('GET /cart/:id', () => {
-  it('endpoint: GET /cart/:id', (done) => {
-    const call = {
-      url: 'http://localhost:7865/cart/12',
-      method: 'GET',
-    };
-    request(call, (error, response, body) => {
-      chai.expect(response.statusCode).to.equal(200);
-      chai.expect(body).to.equal('Payment methods for cart 12');
-      done();
-    });
-  });
-});
-
-describe('GET /cart/:isNaN', () => {
-  it('endpoint: GET /cart/:isNaN', (done) => {
-    const call = {
-      url: 'http://localhost:7865/cart/anything',
-      method: 'GET',
-    };
-    request(call, (error, response, body) => {
-      chai.expect(response.statusCode).to.equal(404);
-      done();
-    });
-  });
-});
-
-describe('GET /available_payments', () => {
-  it('endpoint: GET /available_payments', (done) => {
-    const call = {
-      url: 'http://localhost:7865/available_payments',
-      method: 'GET',
-    };
-    request(call, (error, response, body) => {
-      chai.expect(response.statusCode).to.equal(200);
-      chai.expect(body).to.equal(
-        '{"payment_methods":{"credit_cards":true,"paypal":false}}'
-      );
-      done();
-    });
-  });
-});
-
-describe('POST /login', () => {
-  it('POST /login', (done) => {
-    const call = {
-      url: 'http://localhost:7865/login',
-      method: 'POST',
-      json: {
-        userName: 'Javi',
-      },
-    };
-    request(call, (error, response, body) => {
-      chai.expect(response.statusCode).to.equal(200);
-      chai.expect(body).to.equal('Welcome Javi');
-      done();
+      request(options, function (error, response, body) {
+        expect(response.statusCode).to.equal(200);
+        expect(body).to.equal('Welcome to the payment system');
+        done();
+      });
     });
   });
 });
