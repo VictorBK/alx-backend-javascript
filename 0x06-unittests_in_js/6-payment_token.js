9335eb9f80c8b1c4db88bed7981e4b36a593f965
VictorBK <victorbrian4@gmail.com>
2023-02-25 03:18:42 +0300

    Async tests with done mended

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -1,5 +1,13 @@
-'use strict';
+/*
+ * Async tests with done 
+ * @author Yosef Samuel
+ * @param {boolean} success
+ * @return {object} - resolved promise object
+ */
 function getPaymentTokenFromAPI(success) {
-  if (success) return Promise.resolve({ data: 'Successful response from the API' });
+  if (success === true)
+    return Promise.resolve({ data: 'Successful response from the API' });
 }
+
 module.exports = getPaymentTokenFromAPI;
+
--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,5 +1,19 @@
-'use strict';
-function getPaymentTokenFromAPI(success) {
-  if (success) return Promise.resolve({ data: 'Successful response from the API' });
-}
-module.exports = getPaymentTokenFromAPI;
+/*
+ * test suit for 6-payment_token.js
+ */
+
+const getPaymentTokenFromAPI = require('./6-payment_token');
+var { expect } = require('chai');
+
+describe('getPaymentTokenFromAPI', function () {
+  describe('arg true', function () {
+    it('resolved correctly', function (done) {
+      getPaymentTokenFromAPI(true)
+        .then((res) => {
+          expect(res).to.include({ data: 'Successful response from the API' });
+          done();
+        })
+        .catch((err) => done(err));
+    });
+  });
+});
